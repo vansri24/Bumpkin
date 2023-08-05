70eeacfc3295bc08630a2a230af323f00ef8cbcb
Vanshita Srivastava <83030857+vansri24@users.noreply.github.com>
2023-08-06 01:16:34 +0530

    Assets Folder

---
+++ Assets/scenechanger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenechanger : MonoBehaviour
+{
+    public void playGame()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); }
+
+    public void Back()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); }
+
+    public void Interview()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +2); }
+
+    public void InterBack()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2); }
+
+    public void Classroom()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3 ); }
+
+    public void ClassroomBack()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3); }
+
+    public void Date()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4); }
+
+    public void DateBack()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 4); }
+
+
+}
